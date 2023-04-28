f384eb2015594098d1b16d8571216dfb51b38d81
Rahul Manavalan <rahul.learns.physics@ gmail.com>
2023-04-28 23:22:34 +0200

    Fix orientation in plots.

--- wave.jl
+++ wave.jl
@@ -40,7 +40,7 @@ end
 gif(anim,"figures/wave.gif",fps=5)
 
 # Figure
-f1 = Plots.heatmap(x,t,solution[1:length(x),:],c=:summer,xlabel=L"x",ylabel=L"t")
+f1 = Plots.heatmap(x,t,solution[1:length(x),:]',c=:summer,xlabel=L"x",ylabel=L"t")
 title!("1D Wave equation")
 savefig("figures/contour_wave.png")
 savefig("figures/contour_wave.svg")
